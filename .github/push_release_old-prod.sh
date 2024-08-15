f8cf2b57ab687f447250def06b1d98216acb76a7
mintpic dev <108204467+mintpic@users.noreply.github.com>
2024-08-15 08:06:04 -0400

    Rebuild project and update build files

--- .github/push_release_old-prod.sh
+++ .github/push_release_old-prod.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# experimental 
-# https://github.com/kodadot/nft-gallery/issues/5770 
+# experimental
+# https://github.com/voloview/issues/5770
 
 gh pr list -s merged --json url -B main --jq ".[] | .url" | sed 's/^/- /' $1 | gh pr create -t "release update" -d -H beta -B production -l release --body-file -
